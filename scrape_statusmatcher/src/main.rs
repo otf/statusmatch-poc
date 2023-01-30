3730378532093c972050c1bd68d13c0cf0a81b09
otf <otf@me.com>
2023-01-31 06:53:44 +0900

    [scrape] normalize programs.

--- scrape_statusmatcher/src/main.rs
+++ scrape_statusmatcher/src/main.rs
@@ -1,20 +1,25 @@
 use itertools::Itertools;
 use serde::{Deserialize, Serialize};
-use serde_json::json;
 use std::fs::File;
 use std::io::Write;
 
 #[derive(Serialize, Deserialize)]
 struct Status {
-    id: i64,
+    id: usize,
     name: String,
 }
 
 #[derive(Serialize, Deserialize)]
 struct ProgramAndStatus {
-    id: i64,
+    id: usize,
     name: String,
     statuses: Vec<Status>,
+}
+
+#[derive(Serialize)]
+struct NormalizedProgram {
+    id: usize,
+    name: String,
 }
 
 fn retrieve_program_and_statuses() -> reqwest::Result<Vec<ProgramAndStatus>> {
@@ -25,28 +30,29 @@ fn retrieve_program_and_statuses() -> reqwest::Result<Vec<ProgramAndStatus>> {
     Ok(res)
 }
 
-fn dump_programs(program_and_statuses: &Vec<ProgramAndStatus>) -> anyhow::Result<()> {
-    let programs = program_and_statuses
-        .iter()
-        .unique_by(|x| &x.name)
-        .enumerate()
-        .map(|(i, row)| {
-            json!({
-                "id": i,
-                "name": row.name,
-            })
-        })
-        .collect::<Vec<_>>();
-    let json = json!(programs);
-    let str_json = serde_json::to_string(&json)?;
+fn dump_programs(programs: &Vec<NormalizedProgram>) -> anyhow::Result<()> {
+    let json = serde_json::to_string(&programs)?;
     let mut file = File::create("./program.json")?;
-    file.write_all(str_json.as_bytes())?;
+    file.write_all(json.as_bytes())?;
     file.flush()?;
     Ok(())
 }
 
+fn normalize_programs(program_and_statuses: &Vec<ProgramAndStatus>) -> Vec<NormalizedProgram> {
+    program_and_statuses
+        .iter()
+        .unique_by(|row| &row.name)
+        .enumerate()
+        .map(|(i, row)| NormalizedProgram {
+            id: i,
+            name: row.name.clone(),
+        })
+        .collect()
+}
+
 fn main() -> anyhow::Result<()> {
     let program_and_statuses = retrieve_program_and_statuses()?;
-    dump_programs(&program_and_statuses)?;
+    let programs = normalize_programs(&program_and_statuses);
+    dump_programs(&programs)?;
     Ok(())
 }
