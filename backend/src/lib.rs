656b929af734c0c9aa5b5b2e7d88d770e001f8b3
otf <otf@me.com>
2023-02-07 21:27:19 +0900

    Integrate frontend into backend. #11

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,4 +1,7 @@
+use std::path::PathBuf;
+
 use axum::{routing::get, Router};
+use axum_extra::routing::SpaRouter;
 use sync_wrapper::SyncWrapper;
 
 async fn hello_world() -> &'static str {
@@ -6,8 +9,12 @@ async fn hello_world() -> &'static str {
 }
 
 #[shuttle_service::main]
-async fn axum() -> shuttle_service::ShuttleAxum {
-    let router = Router::new().route("/hello", get(hello_world));
+async fn axum(
+    #[shuttle_static_folder::StaticFolder(folder = "public")] static_folder: PathBuf,
+) -> shuttle_service::ShuttleAxum {
+    let router = Router::new()
+        .route("/api/hello", get(hello_world))
+        .merge(SpaRouter::new("/", static_folder).index_file("index.html"));
     let sync_wrapper = SyncWrapper::new(router);
 
     Ok(sync_wrapper)
