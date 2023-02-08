ef2cdacfbee6c50d81b15b0bc2bed1cee5a49c52
otf <otf@me.com>
2023-02-08 23:48:56 +0900

    [frontend][backend] Make it possible to search programs by name.

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,11 +1,31 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, vec};
 
-use axum::{routing::get, Router};
+use axum::{extract::Query, http::StatusCode, response::IntoResponse, routing::get, Json, Router};
 use axum_extra::routing::SpaRouter;
+use serde::Deserialize;
 use sync_wrapper::SyncWrapper;
 
-async fn hello_world() -> &'static str {
-    "Hello, world! from Rust Backend"
+#[derive(Deserialize)]
+struct SearchQuery {
+    text: String,
+}
+
+async fn search_programs(Query(SearchQuery { text }): Query<SearchQuery>) -> impl IntoResponse {
+    let programs = vec![
+        "Marriott Bonvoy".to_owned(),
+        "Best Western Rewards".to_owned(),
+    ];
+
+    if text.trim().is_empty() {
+        return (StatusCode::OK, Json(vec![]));
+    }
+
+    let result = programs
+        .into_iter()
+        .filter(|p| p.to_lowercase().contains(&text.trim().to_lowercase()))
+        .collect::<Vec<_>>();
+
+    (StatusCode::OK, Json(result))
 }
 
 #[shuttle_service::main]
@@ -13,7 +33,7 @@ async fn axum(
     #[shuttle_static_folder::StaticFolder(folder = "public")] static_folder: PathBuf,
 ) -> shuttle_service::ShuttleAxum {
     let router = Router::new()
-        .route("/api/hello", get(hello_world))
+        .route("/api/programs/search", get(search_programs))
         .merge(SpaRouter::new("/", static_folder).index_file("index.html"));
     let sync_wrapper = SyncWrapper::new(router);
 
--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,31 +1,70 @@
 module Main exposing (..)
 
 import Browser
-import Element exposing (text)
+import Element exposing (Element, text)
 import Element.Input as Input
 import Html exposing (Html)
+import Http
+import Json.Decode exposing (Decoder, list, string)
 
 
 type alias Model =
-    ()
+    { searchText : String
+    , programs : List String
+    }
 
 
-type alias Msg =
-    ()
+type Msg
+    = GotSearchText String
+    | LoadPrograms (Result Http.Error (List String))
+
+
+programsDecoder : Decoder (List String)
+programsDecoder =
+    list string
+
+
+fetchPrograms : String -> Cmd Msg
+fetchPrograms text =
+    Http.get
+        { url = "api/programs/search?text=" ++ text
+        , expect = Http.expectJson LoadPrograms programsDecoder
+        }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update () model =
-    ( model, Cmd.none )
+update msg model =
+    case msg of
+        GotSearchText text ->
+            ( { model
+                | searchText = text
+              }
+            , fetchPrograms text
+            )
+
+        LoadPrograms (Ok programs) ->
+            ( { model
+                | programs = programs
+              }
+            , Cmd.none
+            )
+
+        LoadPrograms (Err _) ->
+            ( model, Cmd.none )
+
+
+initModel : Model
+initModel =
+    { searchText = "", programs = [] }
 
 
 init : () -> ( Model, Cmd Msg )
 init () =
-    ( (), Cmd.none )
+    ( initModel, Cmd.none )
 
 
 subscriptions : Model -> Sub msg
-subscriptions () =
+subscriptions model =
     Sub.none
 
 
@@ -39,12 +78,33 @@ main =
         }
 
 
-view : Model -> Html msg
-view () =
-    Element.layout [] <|
-        Input.text []
-            { onChange = Debug.todo
-            , text = ""
+viewPrograms : List String -> Element Msg
+viewPrograms programs =
+    let
+        children =
+            programs
+                |> List.map (\p -> text p)
+    in
+    Element.column
+        []
+        children
+
+
+viewFrom : Model -> Element Msg
+viewFrom model =
+    Element.column
+        []
+        [ Input.search []
+            { onChange = GotSearchText
+            , text = model.searchText
             , placeholder = Nothing
             , label = Input.labelLeft [] <| text "Search a program."
             }
+        , viewPrograms model.programs
+        ]
+
+
+view : Model -> Html Msg
+view model =
+    Element.layout [] <|
+        viewFrom model
